ee9f12916eda5e035231feeb2cc65530fff235e4
kyh275 <kyh275@nyu.edu>
2021-01-31 16:25:47 -0500

    credit

---
+++ LostAndFound/Assets/Scripts/Creddit.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Creddit : MonoBehaviour
+{
+    GameManager gameManager;
+
+    public Text point;
+    public Text heart;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GetComponent<GameManager>();
+        point.text = gameManager.score.ToString();
+        heart.text = gameManager.nowHeart.ToString();
+    }
+
+    public bool isEnd = false;
+    // Update is called once per frame
+    void Update()
+    {
+        if (isEnd && Input.GetKeyDown(KeyCode.Space))
+            gameManager.EnterNextScene("Title");
+    }
+}
