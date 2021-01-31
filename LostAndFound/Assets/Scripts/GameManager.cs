df73610815cd0296e74d59633b18be45be153332
kyh275 <kyh275@nyu.edu>
2021-01-30 19:49:54 -0500

    add heart and score

--- LostAndFound/Assets/Scripts/GameManager.cs
+++ LostAndFound/Assets/Scripts/GameManager.cs
@@ -6,7 +6,12 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public int heartNum = 4;
+    public float heartDistance = 110f;
+    public GameObject health;
+    public GameObject heartTemplete;
 
+    List<GameObject> heartList = new List<GameObject>();
     private void Awake()
     {
         DontDestroyOnLoad(transform.gameObject);
@@ -15,12 +20,25 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < heartNum; i++)
+            AddHeart();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
+
+    public void AddHeart()
+    {
+        GameObject newHeart = Instantiate(heartTemplete, health.transform);
         
+        newHeart.GetComponent<RectTransform>().anchoredPosition = new Vector3(heartTemplete.GetComponent<RectTransform>().localPosition.x + heartList.Count * heartDistance, 0.0f);
+        newHeart.SetActive(true);
+        heartList.Add(newHeart);
+    }
+    public void RemoveHeart()
+    {
     }
 }
