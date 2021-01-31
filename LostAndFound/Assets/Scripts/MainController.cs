62ac43bc291315356b0f7abfad207e289f002884
Melvin <wechenme@gmail.com>
2021-01-30 19:49:45 -0500

    isGrounded

--- LostAndFound/Assets/Scripts/MainController.cs
+++ LostAndFound/Assets/Scripts/MainController.cs
@@ -7,35 +7,110 @@ public class MainController : MonoBehaviour
     public float speed = 0.1f;
 
     private Collider2D collider;
-    
+    private Rigidbody2D body2D;
     private float Life = 4.0f;
+    private float isGroundedRayLength =0.1f;
+    private LayerMask layerMaskForGrounded;
+    private float groundHeight = -1.8f;
+    public float standingThreshold = 4f;
+    public bool standing;
+    public float jetSpeed = 150f;
+    public Vector2 maxVelocity = new Vector2 (60,100);
+    
     // Start is called before the first frame update
     void Start()
     {
         collider = GetComponent<BoxCollider2D>();
+        body2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+        var absVelX = Mathf.Abs(body2D.velocity.x);
+        var absVelY = Mathf.Abs(body2D.velocity.y);
+        
+        
+        if( absVelY <= standingThreshold){
+            standing = true;
+        }
+        else{
+            standing = false;
+        }
+        var forceX = 0f;
+        var forceY = 0f;
+        Debug.Log("absVelY:"+absVelY);
+        Debug.Log("absVelX:"+absVelX);
+        if (isGround() && Input.GetKeyDown(KeyCode.Space)){
+            if(absVelY<maxVelocity.y){
+                forceY = jetSpeed;
+            }
+
+        }
+        
+
+        body2D.AddForce(new Vector2(forceX, forceY));
+        
+        /*
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
         Vector2 position = transform.position;
         position.x = position.x + speed * h;
         position.y = position.y + speed * v;
-        transform.position = position;
+        transform.position = position;*/
+        
+        //finite state machine
+        //isground && key press -> jump -> jump2 -> run
+        //bool a = isGround();
+
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         print("collision!!!!");
-        other.GetComponent<Token>().PlayerTouched();
+        if(other.GetComponent<Token>() != null){
+            other.GetComponent<Token>().PlayerTouched();
+            if(other.GetComponent<Obstacle>() != null){
+                damage();
+                print("damage!");
+                printLife();
+            }
+            if(other.GetComponent<Point>() != null){
+                heal();
+                print("heal");
+                printLife();
+            }
+        }
+    }
+    private bool isGround(){
+        Vector3 position = transform.position;
+        position.y = this.GetComponent<Collider2D>().bounds.min.y + 0.1f;
+        //float length = isGroundedRayLength + 0.1f;
+        //bool grounded = Physics2D.Raycast (position, Vector3.down, length, layerMaskForGrounded.value);
+        
+        //Debug.Log("isGround positiony:" + position.y);
+        bool grounded = true;
+        if(position.y >= groundHeight - 0.1f && position.y <= groundHeight + 0.1f){
+            Debug.Log("is on the Ground!");
+        }else{
+            Debug.Log("not on the ground!");
+            grounded = false;
+        }
+        
+        return grounded;
+    }
+    private void jump(){
+        //
     }
     public void damage(){
         this.Life -= 0.5f;
     }
-    public void health(){
+    public void heal(){
         this.Life += 0.5f;
+    }
+    public void printLife(){
+        print("Player life:" + this.Life);
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
